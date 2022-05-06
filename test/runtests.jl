bf59b31f633f0910b08f07bb772fe94797e03edf
Graham Stark <graham.stark@virtual-worlds.biz>
2022-05-06 09:43:21 +0100

    testcase and missing GLC files

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,6 +3,6 @@ using Test
 
 @testset "STV.jl" begin
     # Write your tests here.
-    fname = "doc/";
+    fname = "../docs/2017/PreferenceProfile_Report_Ward_7_Langside_05052017_163223.xlsx"
     STV.do_election( fname )
 end

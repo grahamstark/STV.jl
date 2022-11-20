958ff9b9b5ed89e0eac4dbf2b3e1b68304fdeb21
Graham Stark <graham.stark@virtual-worlds.biz>
2022-11-20 22:50:37 +0000

    Linn By-Election stuff

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,7 +5,8 @@ using Plots
 
 @testset "STV.jl" begin
     # Write your tests here.
-    fname = "../docs/2017/PreferenceProfile_Report_Ward_7_Langside_05052017_163223.xlsx"
+    # fname = "../docs/2017/PreferenceProfile_Report_Ward_7_Langside_05052017_163223.xlsx"
+    fname = "../docs/Linn/Ward-1-Linn-reports-for-publication/PreferenceProfile_V0001_Ward-1-Linn_18112022_002249.blt"
     candidates,wardname,quota,votes,transfers,elected,excluded,seats,stages = STV.do_election( fname )
     p = make_sankey( wardname, candidates, votes, transfers, elected, excluded, stages )
 end
